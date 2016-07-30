59e5c5e7f212a9bfd99216af54cba96bc2054959
Samuel Hindmarsh <samuel.hindmarsh@gmail.com>
2016-07-31 10:33:34 +1200

    Changed compton config, add fonts config. Changed monospace font to Tamzen. Replaced yaourt-cleandeps alias with function

--- my.zsh
+++ my.zsh
@@ -1,4 +1,7 @@
 export PATH=$PATH:~/scripts:~/.gem/ruby/2.3.0/bin
+
 alias hc=herbstclient
 alias poweroff="systemctl poweroff"
 alias reboot="systemctl reboot"
+
+function yaourt-cleandeps() { yaourt -Rns $(yaourt -Qtdq) }
