f16b1f83cad9d41291ad3ed97747fd9afc769cd5
Sam Hindmarsh <samuel.hindmarsh@gmail.com>
2016-08-10 19:07:35 +1200

    Adding keychain to zsh startup script

--- my.zsh
+++ my.zsh
@@ -1,5 +1,8 @@
 export PATH=$PATH:~/scripts:~/.gem/ruby/2.3.0/bin
 
+# Load ssh keys (working directory is ~/.ssh/)
+eval $(keychain --eval --quiet id_rsa)
+
 alias hc=herbstclient
 alias poweroff="systemctl poweroff"
 alias reboot="systemctl reboot"
